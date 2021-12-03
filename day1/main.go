8a6337e6220d95c620592fdb049b364d4cf84d79
Seth Littleford <seth.littleford@gmail.com>
2021-12-03 16:34:46 -0700

    refactor day 1 using utility helpers

--- day1/main.go
+++ day1/main.go
@@ -1,33 +1,23 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"path/filepath"
-	"strconv"
+
+	"github.com/sethlittleford/aoc-2021/utils"
 )
 
 func main() {
-	absPath, err := filepath.Abs("day1/input.txt")
-	check(err)
+	report := utils.ReadInts("day1/input.txt")
 
-	fmt.Printf("Part 1 Answer: %d\n", part1(absPath))
-	fmt.Printf("Part 2 Answer: %d\n", part2(absPath))
+	fmt.Printf("Part 1 Answer: %d\n", part1(report))
+	fmt.Printf("Part 2 Answer: %d\n", part2(report))
 }
 
-func part1(filePath string) int {
-	file, err := os.Open(filePath)
-	check(err)
-	defer file.Close()
+func part1(input []int) int {
 
-	scanner := bufio.NewScanner(file)
 	var answer int
 	var previousDepth int
-	for scanner.Scan() {
-		depthMeasurement, err := strconv.Atoi(scanner.Text())
-		check(err)
-
+	for _, depthMeasurement := range input {
 		if depthMeasurement > previousDepth {
 			answer++
 		}
@@ -40,24 +30,10 @@ func part1(filePath string) int {
 	return answer
 }
 
-func part2(filePath string) int {
-	file, err := os.Open(filePath)
-	check(err)
-	defer file.Close()
-
-	measurements := make([]int, 0)
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		depthMeasurement, err := strconv.Atoi(scanner.Text())
-		check(err)
-
-		measurements = append(measurements, depthMeasurement)
-	}
-
+func part2(measurements []int) int {
 	windowSums := make([]int, 0)
-	for i := 1; i < len(measurements) - 1; i++ {
-		currentSum := measurements[i - 1] + measurements[i] + measurements[i + 1]
+	for i := 1; i < len(measurements)-1; i++ {
+		currentSum := measurements[i-1] + measurements[i] + measurements[i+1]
 		windowSums = append(windowSums, currentSum)
 	}
 
@@ -66,17 +42,11 @@ func part2(filePath string) int {
 	for _, currentSum := range windowSums {
 		if currentSum > previousSum {
 			answer++
-		} 
+		}
 		previousSum = currentSum
 	}
-	
+
 	answer -= 1
 
 	return answer
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sethlittleford/aoc-2021
+
+go 1.16
