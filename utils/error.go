f9bb50711e075d551e3f2a0bc0845825e40ec069
Seth Littleford <seth.littleford@gmail.com>
2021-12-03 16:18:56 -0700

    make CheckErr a variadic function

--- utils/error.go
+++ utils/error.go
@@ -2,13 +2,16 @@ package utils
 
 import (
 	"errors"
-	"fmt"
 )
 
 // CheckErr is a helper function that checks if the given error
-// is non-nil. If so, it appends the custom error message to the error and panics
-func CheckErr(err error, message string) {
+// is non-nil. If so, it appends custom error message(s) to the error and panics
+func CheckErr(err error, message ...string) {
 	if err != nil {
-		panic(errors.New(fmt.Sprintf("%s | %s", err.Error(), message)))
+		s := err.Error()
+		for _, msg := range message {
+			s += " | " + msg
+		}
+		panic(errors.New(s))
 	}
 }
