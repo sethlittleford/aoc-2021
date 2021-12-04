8069efb350bbadc58364eeb2639ae11f7b137b0f
Seth Littleford <seth.littleford@gmail.com>
2021-12-03 20:52:58 -0700

    utility function to read input file as strings

--- utils/file.go
+++ utils/file.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-// ReadInts reads the file at the relative filePath into memory as a slice of int
+// ReadInts reads the file at the relative filePath into memory as a slice of ints
 func ReadInts(filePath string) []int {
 	absPath, err := filepath.Abs(filePath)
 	CheckErr(err, "failed to return absolute file path representation")
@@ -23,6 +23,25 @@ func ReadInts(filePath string) []int {
 		i, err := strconv.Atoi(scanner.Text())
 		CheckErr(err, "could not convert string to int while reading file")
 		result = append(result, i)
+	}
+
+	return result
+}
+
+// ReadStrings reads the file at the relative filePath into memory as a slice of strings
+func ReadStrings(filePath string) []string {
+	absPath, err := filepath.Abs(filePath)
+	CheckErr(err, "failed to return absolute file path representation")
+
+	file, err := os.Open(absPath)
+	CheckErr(err, "could not open file at: ", absPath)
+	defer file.Close()
+
+	result := make([]string, 0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		result = append(result, scanner.Text())
 	}
 
 	return result
